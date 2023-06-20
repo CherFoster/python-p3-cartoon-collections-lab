63b62f3ab4015ba9d09a57d7ccb724ec007acf68
Cherise Foster <cherise.foster@outlook.com>
2023-06-20 11:28:27 -0600

    passed tests

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,20 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for i, name in enumerate(names, 1):
+        print(f"{i}. {name}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    capitalized = [calls.capitalize()+"!" for calls in planeteer_calls]
+    return capitalized
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(list):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for cheese in list:
+        if cheese in cheeses:
+            return cheese
+    return None
